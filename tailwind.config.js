33a6bbc4a26e441ddf0ce73ae4a592d77969ef23
Mark Joshwel <mark@joshwel.co>
2022-11-30 06:48:16 +0000

    site,config: move src file + reflect in configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: ["./src/web/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
