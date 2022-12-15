92639de35151ab29554165ab33a82216ccdad725
Mark Joshwel <mark@joshwel.co>
2022-12-15 07:51:43 +0000

    cfg: also check for public html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  content: ["./src/web/**/*.html"],
+  content: ["./src/web/**/*.html", "./public/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['font-text', ...defaultTheme.fontFamily.sans],
+        "sans": ["font-text", ...defaultTheme.fontFamily.sans],
       },
     },
   },
+  safelist: [
+    "list-none",
+    "list-disc",
+    "list-decimal",
+  ]
 }
